92291c4fe6314135bdb78a685d9105ad2a6cc583
dd <1106541135@qq.com>
2017-10-12 16:35:05 +0800

    添加数据库迁移

--- database/migrations/2016_11_11_163610_create_taggables_table.php
+++ database/migrations/2016_11_11_163610_create_taggables_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateTaggablesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token')->index();
-            $table->timestamp('created_at')->nullable();
+        Schema::create('taggables', function (Blueprint $table) {
+            $table->integer('tag_id')->unsigned()->index();
+            $table->integer('taggable_id')->unsigned()->index();
+            $table->string('taggable_type')->index();
         });
     }
 
@@ -27,6 +27,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('password_resets');
+        Schema::dropIfExists('taggables');
     }
 }
