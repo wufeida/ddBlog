46b7c8316c9b8d332977f91e2f124241a344e345
dd <1106541135@qq.com>
2017-12-13 14:26:23 +0800

    修改后台菜单显示顺序

--- resources/views/admin/layous/nav.blade.php
+++ resources/views/admin/layous/nav.blade.php
@@ -37,9 +37,9 @@
                     <li mid="tab7" funurl="{{url('dd/comment')}}"><a>评论管理</a></li>
                     <li mid="tab8" funurl="{{url('dd/visitor')}}"><a>访问列表</a></li>
                     <li mid="tab9" funurl="{{url('dd/file')}}"><a>文件管理</a></li>
-                    <li mid="tab10" funurl="{{url('dd/trash')}}"><a>回收站</a></li>
+                    <li mid="tab10" funurl="{{url('dd/note')}}"><a>便签管理</a></li>
                     <li mid="tab11" funurl="{{url('dd/config')}}"><a>基本配置</a></li>
-                    <li mid="tab12" funurl="{{url('dd/note')}}"><a>便签管理</a></li>
+                    <li mid="tab12" funurl="{{url('dd/trash')}}"><a>回收站</a></li>
                 </ul>
             </li>
         </ul>
