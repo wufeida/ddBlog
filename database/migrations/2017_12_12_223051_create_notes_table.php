04bbc45a3447c7f3aaf3e303f33ef034d39b0f6b
dd <1106541135@qq.com>
2017-12-22 15:56:49 +0800

    修改数据库

--- database/migrations/2017_12_12_223051_create_notes_table.php
+++ database/migrations/2017_12_12_223051_create_notes_table.php
@@ -17,7 +17,7 @@ class CreateNotesTable extends Migration
             $table->increments('id');
             $table->string('name')->nullable()->comment('便签名称');
             $table->text('content')->nullable()->comment('便签内容');
-            $table->boolean('status')->default(0)->comment('便签状态，1表示已处理完成，2表示处理中，0表示未处理');
+            $table->boolean('status')->default(3)->comment('便签状态，1表示已处理完成，2表示处理中，3表示未处理');
             $table->timestamps();
             $table->softDeletes();
         });
